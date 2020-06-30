8ee1eb296f8d5f6cff0cf37d6ef5071cd864c16d
tuanNguyen-Ted <62045637+tuanNguyen-Ted@users.noreply.github.com>
2020-06-30 20:37:53 +0700

    sky's say oh yeah (#1)

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ console.log("dev update project . My name is Sang")
 console.log("dev update project");
 console.log("Tuấn cao 1.8m")
 
+console.log("Người theo hương hoa mây mù dăn lối");
+console.log("SKY's");
